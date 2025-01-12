2479b02de2b77cc4d04b05d371629db3fdba5a92
Temurmirzo Abdukarimov <114911047+devtemPythonist@users.noreply.github.com>
2025-01-12 20:14:22 +0500

    devtemuz

--- devtemfrontend/src/views/layouts/NavbarView.vue
+++ devtemfrontend/src/views/layouts/NavbarView.vue
@@ -1,19 +1,42 @@
 <template>
-    <div>
-      <v-app-bar
-        id="main-header"
-        app
-        height="60px"
-        class="pa-0"
-        :prominent="false"
+  <div>
+    <v-app-bar
+      id="main-header"
+      app
+      height="60px"
+      class="pa-0"
+      :prominent="false"
+    >
+      <router-link to="/">
+        <v-btn>Logo</v-btn>
+      </router-link>
+      <v-spacer></v-spacer>
+      <v-sheet
+        class="center-header"
+        max-width="50%"
+        style="display: flex; justify-content: center; align-items: center"
       >
-        <router-link to="/">
-          <v-btn>Logo</v-btn>
-        </router-link>
-        <v-spacer></v-spacer>
-        <h1>Navbar</h1>
-      </v-app-bar>
-    </div>
+        <v-slide-group show-arrows>
+          <v-slide-group-item
+            v-for="n in 25"
+            :key="n"
+            v-slot="{ isSelected, toggle }"
+          >
+            <v-btn
+              :color="isSelected ? 'primary' : undefined"
+              class="ma-2"
+              rounded
+              @click="toggle"
+            >
+              Options {{ n }}
+            </v-btn>
+          </v-slide-group-item>
+        </v-slide-group>
+      </v-sheet>
+      <v-spacer></v-spacer>
+      <h1>Navbar</h1>
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
@@ -31,5 +54,25 @@ export default {
   left: 0 !important;
   background: #fff;
   box-shadow: 0 1px 4px rgba(0, 0, 0, 0.2);
+}
+.center-header {
+  left: 0 !important;
+  height: 60px;
+}
+.center-header_text {
+  text-align: center;
+  width: auto;
+  line-height: 1.4em;
+  overflow: hidden;
+  text-decoration: inherit;
+  -o-text-overflow: ellipsis;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  cursor: pointer;
+  /* font-family: Roboto, Trebuchet MS, Helvetica, sans-serif; */
+  font-family: "Inter", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  /* font-family: Public sans-serif; */
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.6);
 }
 </style>
